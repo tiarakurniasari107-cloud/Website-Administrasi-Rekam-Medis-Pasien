89dc25f7b7fdb12daf921e7ead68a8117eac7757
tiarakurniasari107-cloud <tiarakurniasari107@gmail.com>
2026-04-25 22:07:02 +0700

    styling

--- includes/header.php
+++ includes/header.php
@@ -12,11 +12,13 @@ $extraHead = isset($extraHead) ? (string) $extraHead : '';
 ?>
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle, ENT_QUOTES, 'UTF-8'); ?></title>
     <link rel="stylesheet" href="<?= htmlspecialchars($cssPath, ENT_QUOTES, 'UTF-8'); ?>">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <?= $extraHead; ?>
 </head>
 <body<?= $bodyAttributes !== '' ? ' ' . $bodyAttributes : ''; ?>>
