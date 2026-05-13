ec9c3ebae4f1a475970098f9790453ceb2615389
Azriel Yalsha Hanifah <124250058@student.upnyk.ac.id>
2026-05-13 07:22:32 +0700

    logout fix

--- includes/header.php
+++ includes/header.php
@@ -53,9 +53,6 @@ unset($_SESSION['flash_error']);
         </div>
         <div class="clinic-header-right">
             <?php if (isset($_SESSION['user_id'])): ?>
-                <span class="user-info">
-                    Halo, <?= htmlspecialchars($_SESSION['user_name'] ?? 'User', ENT_QUOTES, 'UTF-8'); ?>
-                </span>
                 <a href="../auth/logout.php" class="btn btn-logout">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
